07de5bd9e3f0c3d8923e4e7e7753dc678421604f
Andrew Stewart Gibson <tinycode2@gmail.com>
2022-10-13 21:04:07 +0100

    log in and out

--- public/0.1.1/js/app.js
+++ public/0.1.1/js/app.js
@@ -1,8 +1,91 @@
-import { nav } from "./views.js"
+import "/__/firebase/9.12.0/firebase-app-compat.js";
+import "/__/firebase/9.12.0/firebase-auth-compat.js";
+import "/__/firebase/init.js?useEmulator=true";
+import { nav, main } from "./views.js"
+
+async function googleSignIn(app) {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const auth = firebase.auth(app);
+    await auth.signInWithPopup(provider);
+}
+
+async function emailSignIn(app) {
+    const auth = firebase.auth(app);
+    const email = window.prompt("email");
+    const password = window.prompt("password");
+    await auth.signInWithEmailAndPassword(email, password);
+}
+
+function handleClick(app, target) {
+
+    if (target.classList.contains("sign-in")) {
+        const authStrategy = target.classList.contains("google") ? googleSignIn : emailSignIn;
+        return async function signIn() {
+            await authStrategy(app);
+        }
+    }
+    if (target.classList.contains("sign-out")) {
+        return async function signOut() {
+            await firebase.auth(app).signOut();
+        }
+    }
+}
+
+const model = {};
+
+const domParser = new DOMParser();
+
+function renderNav(app) {
+    const doc = domParser.parseFromString(nav(model), "text/html");
+    const rendered = doc.body.querySelector("nav");
+    document.querySelector("body > nav").replaceWith(rendered);
+    registerListeners(rendered, app);
+}
 
 (async function initialize() {
 
-    const body = nav();
-    document.body.innerHTML = body;
+
+    try {
+        let app = firebase.app();
+        model.features = [
+            'auth',
+            'database',
+            'firestore',
+            'functions',
+            'messaging',
+            'storage',
+            'analytics',
+            'remoteConfig',
+            'performance',
+        ].filter(feature => typeof app[feature] === 'function');
+
+        const body = [
+            nav(model),
+            main(model)
+        ].join("");
+        document.body.innerHTML = body;
+        const container = document.body;
+        registerListeners(container, app);
+
+        app.auth().onAuthStateChanged(user => {
+            if (model.user === user) return;
+            model.user = user;
+            renderNav(app);
+
+        });
+
+    } catch (e) {
+
+        console.error(e);
+        document.body.innerHTML = 'Error loading the Firebase SDK, check the console.';
+
+    }
+
 
 }())
+
+function registerListeners(container, app) {
+    for (const button of container.querySelectorAll("button")) {
+        button.addEventListener("click", handleClick(app, button));
+    }
+}
--- public/0.1.1/js/views.js
+++ public/0.1.1/js/views.js
@@ -1,9 +1,45 @@
-export function nav() {
+
+function buildRoot() {
+    const url = new URL(import.meta.url);
+    url.pathname = url.pathname.replace(/\/js\/views.js$/, "");
+    return url.toString();
+}
+const root = buildRoot()
+
+function signIn() {
+
+    return `
+        <button class="sign-in google">Sign in (Google)</button>
+        <button class="sign-in email">Sign in (email)</button>
+    `;
+
+}
+
+function acknowledgeUser({ email }) {
+
+    return `
+        ${email}
+        <button class="sign-out">Sign out</button>
+    `;
+
+}
+
+export function nav({ user }) {
 
     return `
         <nav>
-            <a href="/home.html">Home</a>
+            <a href="${root}/app/home.html">Home</a>
+            <div class="status">${user?.email ? acknowledgeUser(user) : signIn()}</div>
         </nav>
+    `;
+
+}
+export function main(model) {
+
+    return `
+        <main>
+            Using Firebase features: ${model.features.join(", ")}
+        </main>
     `;
 
 }
